05e33cd2ca9d4580f14ef053966cb77303fc895d
洪亞偉 <yaweihong@icloud.com>
2016-08-30 20:30:38 +0800

    add api

--- CERRUTI.py
+++ CERRUTI.py
@@ -6,9 +6,19 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class AppHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("app.html")
+
+class OAuthHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("oauth.html")
+
 application = tornado.web.Application(
     [
-    (r"/", MainHandler),
+        (r"/", MainHandler),
+        (r"/app", AppHandler),
+        (r"/oauth", OAuthHandler),
     ],
     static_path=os.path.join(os.path.dirname(__file__), "static"),
 )
