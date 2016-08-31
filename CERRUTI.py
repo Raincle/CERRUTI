d96b7c8af494262493a4cd152c8ae0cddf052bd5
洪亞偉 <yaweihong@icloud.com>
2016-08-31 20:37:42 +0800

    test

--- CERRUTI.py
+++ CERRUTI.py
@@ -1,6 +1,7 @@
 import os.path
 import tornado.ioloop
 import tornado.web
+from tornado import gen,httpclient
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -11,8 +12,20 @@ class AppHandler(tornado.web.RequestHandler):
         self.redirect("http://curio.im/oapi/authorize?redirect_uri=http://cerruti.ahashike.com/oauth&client_id=e8c4600a-0c80-4fdb-835b-f2800d44655d&response_type=code")
 
 class OAuthHandler(tornado.web.RequestHandler):
+    @gen.coroutine
     def get(self):
-        self.render("oauth.html")
+        http_client = httpclient.AsyncHTTPClient()
+        code = self.get_query_argument("code", default="")
+        print(code)
+        url = "http://curio.im/oapi/access_token?client_id=e8c4600a-0c80-4fdb-835b-f2800d44655d&client_secret=ea7d7878-1501-44d1-aaeb-e60f92ba3529&redirect_uri=http://cerruti.ahashike.com/oauth&grant_type=authorization_code&code="+code
+        request = httpclient.HTTPRequest(url, method="GET")
+        response = yield http_client.fetch(request)
+        print(response)
+        if response.status == "200":
+            self.write("SUCCESS")
+        else:
+            self.write("SOME_ERROR_OCCURED")
+
 
 application = tornado.web.Application(
     [
